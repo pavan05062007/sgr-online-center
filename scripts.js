1ff04006128682407e254fdfefb3285cce31650a
PAVANKUMAR ROGANNAVAR <pavanrogannavar@gmail.com>
2026-07-01 23:48:40 +0530

    Add event listener and call functions for contact

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to Shri Guru Raghavendra Online Center");
+});
+
+function callNow() {
+    window.location.href = "tel:9901043113";
+}
+
+function whatsapp() {
+    window.open("https://wa.me/919901043113", "_blank");
+}
